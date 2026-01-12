2f6d7ffad86ea66547495cfcba8df013fd452d3f
오예지 <dalpiecel7@naver.com>
2026-01-12 23:33:02 +0900

    chapter03

---
+++ React/section03/src/index.js
@@ -0,0 +1,14 @@
+// CJS
+// const moduleData = require("./math");
+// console.log(moduleData.add(1, 2));
+// console.log(moduleData.sub(1, 2));
+
+// import mul from "./math.js";
+// import mul, { add, sub } from "./math.js";
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
